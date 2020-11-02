b81eaa10c492cf39fd917fd56ed73126c1e01cbe
Marcelo Diament <marcelodiament@gmail.com>
2020-11-02 00:53:02 -0300

    feature: Vitrine applied to SobrePage and HomePage

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import PageHeader from '../../components/PageHeader'
+import Vitrine from '../../components/Vitrine'
+import { getTopics } from '../../api'
 
 const HomePage = () => {
+  const topics = getTopics()
   return (
     <>
       <PageHeader title="Página Inicial" intro="Página principal, com as novidades e destaques" />
       <h1>Home Page</h1>
+      {topics.map((topic, index) => <Vitrine {...topic} key={index} />)}
     </>
   )
 }
--- frontend/src/pages/SobrePage/index.js
+++ frontend/src/pages/SobrePage/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import PageHeader from '../../components/PageHeader'
 import Vitrine from '../../components/Vitrine'
-import { getTopics, getTopicsByName } from '../../api'
-import { useParams } from 'react-router-dom'
+import { getTopics } from '../../api'
 
 const SobrePage = () => {
-  let params = useParams()
-  const { topicName } = params
   const topics = getTopics()
-  const topicsByName = topicName ? getTopicsByName(topicName) : undefined
 
   return (
     <>
@@ -17,11 +13,7 @@ const SobrePage = () => {
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum modi hic, ipsa blanditiis non eligendi voluptatibus quod itaque? Accusamus laborum itaque, provident vero eveniet totam. Odit exercitationem facilis architecto? Inventore.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum modi hic, ipsa blanditiis non eligendi voluptatibus quod itaque? Accusamus laborum itaque, provident vero eveniet totam. Odit exercitationem facilis architecto? Inventore.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum modi hic, ipsa blanditiis non eligendi voluptatibus quod itaque? Accusamus laborum itaque, provident vero eveniet totam. Odit exercitationem facilis architecto? Inventore.</p>
-      {
-        topicsByName === undefined
-          ? topics.map((topic, index) => <Vitrine {...topic} key={index} />)
-          : topicsByName.map((topic, index) => <Vitrine {...topic} key={index} />)
-      }
+      {topics.map((topic, index) => <Vitrine {...topic} key={index} />)}
     </>
   )
 }
