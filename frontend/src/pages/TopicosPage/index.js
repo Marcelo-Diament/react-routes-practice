8e3d55b4eb29c34fdd83a9c573fb839c387c7c0f
Marcelo Diament <marcelodiament@gmail.com>
2020-11-02 00:46:24 -0300

    feature: vitrine component created and applied to TopicosPage

--- frontend/src/pages/TopicosPage/index.js
+++ frontend/src/pages/TopicosPage/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import PageHeader from '../../components/PageHeader'
+import Vitrine from '../../components/Vitrine'
+import { getTopics, getTopicsByName } from '../../api'
+import { useParams } from 'react-router-dom'
 
 const TopicosPage = () => {
+  let params = useParams()
+  const { topicName } = params
+  const topics = getTopics()
+  const topicsByName = topicName ? getTopicsByName(topicName) : undefined
   return (
     <>
-      <PageHeader title="Página Tópicos" intro="Página que exibe artigos organizados por cada um dos tópicos" />
-      <h1>Topicos Page</h1>
+      <PageHeader title="Tópicos" intro="Navegue por tópicos" />
+      {
+        topicsByName === undefined
+          ? topics.map((topic, index) => <Vitrine {...topic} key={index} />)
+          : topicsByName.map((topic, index) => <Vitrine {...topic} key={index} />)
+      }
     </>
   )
 }
